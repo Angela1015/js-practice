55513ed9743336d1ffdea3eb14fff403f6a986e1
Angela1015 <49008406+Angela1015@users.noreply.github.com>
2019-10-25 20:21:33 -0500

    completed keyless car exercises

---
+++ DeBerryKeylessCar2.js
@@ -0,0 +1,15 @@
+let checkDriverAge = function(age){
+    if
+  (Number(age) < 18) {
+              alert("Sorry, you are too young to drive this car. Powering off");
+  }
+  else if (Number(age) > 18) {
+              alert("Powering On. Enjoy the ride!");
+  }
+  else if (Number(age) === 18) {
+              alert("Congratulations on your first year of driving. Enjoy the ride!");
+  }
+  }
+  
+  checkDriverAge(75)
+  
