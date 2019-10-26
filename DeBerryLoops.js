8430ea38e26ea83f544d9e6b75540727a8f22c63
Angela1015 <49008406+Angela1015@users.noreply.github.com>
2019-10-26 15:57:38 -0500

    added loops exercise

---
+++ DeBerryLoops.js
@@ -0,0 +1,12 @@
+for(let i=0;i<21;i++)
+if(i%2===0){
+    alert(`${i} is even`);
+    
+}else{
+    alert(`${i} is odd`)
+}
+
+for(let i=0;i<11;i++){
+    let product = i*9
+    console.log(`${i}*9 =${product}`)
+}
